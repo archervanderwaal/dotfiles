081cf745b04a5dbbb0f5420655bee84f8b2ebddc
mayongbin <mayongbin@meituan.com>
2021-10-08 15:11:44 +0800

    feat: copy jadetrans config file.

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ if [ ! -d "$HOME/.dotfiles" ]; then
     echo "Installing dotfiles for the first time"
     git clone --depth=1 https://github.com/archervanderwaal/dotfiles.git "$HOME/.dotfiles"
     cd "$HOME/.dotfiles"
+    cp jadetrans/jadetrans.yaml ~/.jadetrans/jadetrans.yaml
     rake install
 else 
     echo "dotfiles is already installed"
